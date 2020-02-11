b6baac52fa718f737f1bd60ffbf6e9cf98a7b4ce
Akmal Pratama <m.akmalpratama@yahoo.co.id>
2020-02-12 01:02:31 +0700

    get every query

--- preprocess.py
+++ preprocess.py
@@ -1,38 +1,45 @@
 import io 
 from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize 
+from nltk.tokenize import word_tokenize
 
-f = open("query-indo-eng.trec", "r")
+import os
+
+f = open('query-indo-eng.trec', 'r')
 doc_col = f.read().replace('.', '')
 doc_col = doc_col.replace('?', '')
+doc_col = doc_col.replace(',', '')
 doc_col = doc_col.lower()
 f.close()
 
-f = open("query-indo-eng-out.trec", "w")
+f = open('query-indo-eng-out.trec', 'w')
 f.write(doc_col)
 f.close()
 
-print("#######")
 
 stop_words = set(stopwords.words('english'))
 f = open("query-indo-eng-out.trec") 
 line = f.read()
 words = line.split()
-iter = 0
+if (os.path.exists('query-indo-eng-out-filtered.trec')):
+    os.remove('query-indo-eng-out-filtered.trec')
 for r in words:
     if not r in stop_words:
-        # print(r)
-        # iter += 1
-        # if (iter > 15):
-        #     break
-        appendFile = open('query-indo-eng-out-filtered.trec','a')
-        appendFile.write(" "+r)
-        appendFile.close()
+        if (r.endswith('>')):
+            appendFile = open('query-indo-eng-out-filtered.trec','a')
+            if (r.startswith('</')):
+                appendFile.write('\n' + r + '\n')
+            else:
+                appendFile.write(r + '\n')
+            appendFile.close()
+        else:
+            appendFile = open('query-indo-eng-out-filtered.trec','a')
+            appendFile.write(r+' ')
+            appendFile.close()
 f.close()
-# f = open("../adi/query-indo-eng-out.trec", "r")
-# if f.mode == "r":
-#     contents = f.readlines()
-#     queries = []
-#     for row in contents:
-#         if (row[0]!='<'):
-#             queries.append(row)
+f = open('query-indo-eng-out-filtered.trec', 'r')
+if f.mode == 'r':
+    contents = f.readlines()
+    queries = []
+    for row in contents:
+        if (row[0]!='<'):
+            queries.append(row)
